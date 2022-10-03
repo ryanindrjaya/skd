09cf30b190c18f16ac390777a077e7004d3b2e60
Ryan Indrajaya <sryan062@gmail.com>
2022-10-03 13:36:18 +0700

    dekripsi caesar cipher

--- tugas3/index.php
+++ tugas3/index.php
@@ -10,13 +10,14 @@
 <body>
   <div class='bg-blue-500 h-screen w-full flex justify-center items-center'>
     <!-- Form input plaintext dan key -->
-    <form action="enkcaesar.php" method="get" class="w-1/4 shadow-xl p-8 bg-white rounded-lg border flex flex-col">
+    <form action="enkcaesar.php" method="post" class="w-1/4 shadow-xl p-8 bg-white rounded-lg border flex flex-col">
       <h1 class='text-3xl font-bold text-center mb-6 text-gray-600'>Caesar Cipher</h1>
-      <label class='text-lg mb-3' for="plain_text">Plainteks</label>
+      <label class='text-lg mb-3' for="plain_text">Teks</label>
       <input type="text" class='mb-5 border focus:outline-none py-2 px-2' name='kata' id="plain_text">
       <label class='text-lg mb-3' for="key">Key</label>
       <input type="number" class='mb-5 border focus:outline-none py-2 px-2' maxlength="5" name='key' id="key">
-      <input type="submit" name='submit' class='cursor-pointer bg-blue-400 py-3 px-4 rounded-lg text-white' value='Enkripsi'>
+      <input type="submit" name='submit_enkripsi' class='cursor-pointer bg-blue-400 py-3 px-4 rounded-lg text-white' value='Enkripsi'>
+      <input type="submit" name='submit_dekripsi' class='cursor-pointer bg-blue-400 py-3 mt-5 px-4 rounded-lg text-white' value='Dekripsi'>
     </form>
   </div>
 </body>
